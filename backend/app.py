d714e83d5bd4c4565f7e9af8f34176415c849e72
Priyadarshini777 <mpriyadharu@gmail.com>
2025-11-22 11:01:46 +0530

    changed

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,12 @@
+from flask import Flask, jsonify
 
-from flask import Flask,jsonify
-app=Flask(__name__)
+app = Flask(__name__)
+
 @app.get("/")
-def home(): return jsonify(msg="Backend running on GKE with CI/CD")
+def home():
+    return jsonify(
+        message="Backend running on GKE with CI/CD"
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
